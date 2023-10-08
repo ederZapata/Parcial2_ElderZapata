08741506dab6bc919266e1336960a7be18e65e07
ederZapata <elderzapata72@gmail.com>
2023-10-07 22:58:58 -0500

    created class Entity and class Natural Person

---
+++ Parcial2/Parcial2/DAL/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2.DAL.Entities
+{
+    public class Entity
+    {
+        [Required]
+        public Guid Id { get; set; }
+        
+        public DateTime? CreatedDate { get; set; }
+       
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ Parcial2/Parcial2/DAL/Entities/NaturalPerson.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2.DAL.Entities
+{
+    public class NaturalPerson : Entity
+    {
+        [Display(Name = "Ingrese el nombre completo")]        
+        [Required(ErrorMessage = "¡El campo {0} es requerido!")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Ingrese su email")]       
+        [Required(ErrorMessage = "¡El campo {0} es requerido!")]
+        public string Email { get; set; }
+
+        [Display(Name = "Ingrese fecha de cumpleaños")]        
+        [Required(ErrorMessage = "¡El campo {0} es requerido!")]
+        public string BirthDate { get; set; }
+
+        [Display(Name = "Ingrese su edad")]        
+        [Required(ErrorMessage = "¡El campo {0} es requerido!")]
+        public string Age { get; set; }
+
+
+
+    }
+
+
+
+
+}
+
